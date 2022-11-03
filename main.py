f0485e5e81c9bfe5b76df6cb3c6837035dbdec04
Serojisa551 <116907048+Serojisa551@users.noreply.github.com>
2022-11-04 00:24:25 +0400

    calculator became main

---
+++ main.py
@@ -0,0 +1,101 @@
+# 5.Write a calculator on python.
+def sum(one_number, two_number):
+    modul = one_number + two_number
+    return modul
+
+
+def sub(one_number, two_number):
+    modul = one_number - two_number
+    return modul
+
+
+def mul(one_number, two_number):
+    modul = one_number * two_number
+    return modul
+
+
+def divide(one_number, two_number):
+    if one_number == 0:
+        "Cannot be divided by zero"
+    else:
+        modul = one_number / two_number
+        return modul
+
+
+def degree(one_number, two_number):
+    if one_number == 0 and two_number == 0:
+        print("0**0 It's impossible")
+    else:
+        modul = one_number**two_number
+        return modul
+
+
+def percent(one_number, two_number):
+    modul = (two_number / one_number) * 100
+    return modul
+
+
+def factorial(one_number):
+    amount = 1
+    if one_number < 0:
+        return "Eror"
+    if one_number == 0:
+        return 1
+    for i in range(1, one_number + 1):
+        amount = amount * i
+    return amount
+
+
+def calc(one_number, operator, two_number):
+    if operator == "+":
+        return sum(one_number, two_number)
+    elif operator == "-":
+        return sub(one_number, two_number)
+    elif operator == "*":
+        return mul(one_number, two_number)
+    elif operator == "/":
+        return divide(one_number, two_number)
+    elif operator == "**":
+        return degree(one_number, two_number)
+    elif operator == "%":
+        return percent(one_number, two_number)
+    elif operator == "!":
+        return factorial(one_number)
+
+
+def mian():
+    one_number = ""
+    two_number = ""
+    operator = ""
+    task = input("input task...")
+    for symbols in task:
+        if 48 <= ord(symbols) <= 57:
+            pass
+        elif 43 == ord(symbols) or 45 == ord(symbols) or 46 == ord(symbols):
+            pass
+        elif 47 == ord(symbols) or 42 == ord(symbols):
+            pass
+        elif 33 == ord(symbols) or 37 == ord(symbols):
+            pass
+        else:
+            return "Unidentified object '{}'".format(symbols)
+    for symbol in task:
+        if symbol in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."):
+            print("two_number", two_number)
+            one_number += symbol
+            two_number += symbol
+        else:
+            one_number += " "
+            two_number += " "
+            operator += symbol
+    one_number = one_number[: one_number.index(" ")]
+    if operator != "!":
+        two_number = two_number[two_number.index(" ") + 1:]
+    one_number = float(one_number)
+    two_number = float(two_number)
+    if operator == "!":
+        one_number = int(one_number)
+    print(calc(one_number, operator, two_number))
+
+
+mian()
