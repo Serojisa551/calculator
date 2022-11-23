d9ad7a4eecc3e550fa5ce8eafb10f65087e65db9
Serojisa551 <116907048+Serojisa551@users.noreply.github.com>
2022-11-23 22:18:52 +0400

    The content of the main function has changed And the data_processing function has been added

--- main.py
+++ main.py
@@ -5,26 +5,25 @@ import clacul.count_functions
 
 def main():
     task = input("input task...")
-    if tasks(task) != None:
-        print(tasks(task))
+    if data_processing(task) != None:
+        print(data_processing(task))
+
+
+def data_processing(task):
+    if task == "":
+        return "You haven't written anything"
+    for symbols in task:
+        if symbols in ("1234567890.-+*/%!"):
+            pass
+        else:
+            return "Unidentified object '{}'".format(symbols)
+    return tasks(task)
 
 
 def tasks(task):
     one_number = ""
     two_number = ""
     operator = ""
-
-    for symbols in task:
-        if 48 <= ord(symbols) <= 57:
-            pass
-        elif 43 == ord(symbols) or 45 == ord(symbols) or 46 == ord(symbols):
-            pass
-        elif 47 == ord(symbols) or 42 == ord(symbols):
-            pass
-        elif 33 == ord(symbols) or 37 == ord(symbols):
-            pass
-        else:
-            return "Unidentified object '{}'".format(symbols)
     for symbol in task:
         if symbol in ("0123456789."):
             one_number += symbol
@@ -36,11 +35,13 @@ def tasks(task):
     one_number = one_number[: one_number.index(" ")]
     if operator != "!":
         two_number = two_number[two_number.index(" ") + 1:]
+
     one_number = float(one_number)
     two_number = float(two_number)
     if operator == "!":
         one_number = int(one_number)
     print(clacul.define_operator.calc(one_number, operator, two_number))
+
 
 if __name__ == "__main__":
     main()
