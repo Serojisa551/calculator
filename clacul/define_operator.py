365f497947a03690dbcdd4aada64e869d14c1740
Serojisa551 <116907048+Serojisa551@users.noreply.github.com>
2022-11-25 03:07:43 +0400

    Changed the sign of the energy operator in the file clacul/define_operator.py

--- clacul/define_operator.py
+++ clacul/define_operator.py
@@ -10,7 +10,7 @@ def calc(one_number, operator, two_number):
         return clacul.count_functions.mul(one_number, two_number)
     elif operator == "/":
         return clacul.count_functions.divide(one_number, two_number)
-    elif operator == "**":
+    elif operator == "^":
         return clacul.count_functions.degree(one_number, two_number)
     elif operator == "%":
         return clacul.count_functions.percent(one_number, two_number)
