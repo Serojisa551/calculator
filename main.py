b1b70c1b53b080d6d45eb34cd92dbb52fcde83e7
Serojisa551 <116907048+Serojisa551@users.noreply.github.com>
2022-11-28 17:42:35 +0400

    fixed the percent operator bug

--- main.py
+++ main.py
@@ -31,6 +31,8 @@ def data_processing(task):
         return "You wrote the wrong number of operators '{}'".format(task)
     elif symbol == len(task) - 1 and task[symbol] != "!":
         return "You haven't written enough '{}'".format(task)
+    # elif task[0] == "-" and  symbol == len(task) -2 and task[symbol] != "!":
+    #     return "You haven't written enough '{}'".format(task)
     return separation_elements(task)
 
 
@@ -52,7 +54,9 @@ def separation_elements(task):
     one_number = one_number[: one_number.index(" ")]
     if operator != "!":
         two_number = two_number[two_number.index(" ") + 1 :]
-
+    if operator == "%":
+        if one_number == "0":
+            return "This is important, I do not divide it by zero"
     one_number = float(one_number)
     two_number = float(two_number)
     if operator == "!":
